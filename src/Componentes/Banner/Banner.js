4023faf528bad0780b204584848df91bfb26c9f2
caique <kaikejrsilva55@gmail.com>
2024-09-02 21:24:35 -0300

    Iniciando projeto de organograma da empresa. Criando banner o formulario

---
+++ src/Componentes/Banner/Banner.js
@@ -0,0 +1,9 @@
+import './Banner.css'
+export const  Banner =()=>{
+    //JSX - Html por tras , fazendo um apeend no html.
+    return(
+        <header className="banner">
+             <img src="/imagens/banner.png" alt="O banner principal da pagina Organo"/>
+        </header>
+    )
+}
